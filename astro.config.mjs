c7b59243990c941f17570eeaa7f4acf8ede58d79
Szarvas Péter <peterszarvas94@gmail.com>
2023-01-09 14:26:10 +0100

    add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
