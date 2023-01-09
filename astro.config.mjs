6ec28bb37b5d44225a5b30236ee8d270e91ef851
Szarvas Péter <peterszarvas94@gmail.com>
2023-01-09 20:56:22 +0100

    modify mobile menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel()
